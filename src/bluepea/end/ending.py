fa2ae3a345b5d63ff10a8a43d4fc7ead24ee0715
Samuel M Smith <smith.samuel.m@gmail.com>
2017-06-26 11:04:27 -0600

    added placeholder for api docs

---
+++ src/bluepea/end/ending.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+"""
+Ending Module
+
+ReST endpoints
+
+"""
+from __future__ import generator_stop
+
+from collections import OrderedDict as ODict, deque
+import enum
+
+from ioflo.aid.sixing import *
+from ioflo.aid import getConsole
+
+console = getConsole()
+
