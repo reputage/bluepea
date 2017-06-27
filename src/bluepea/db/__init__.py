0331084fd32a1a20610acacf6fe431d24c065020
Samuel M Smith <smith.samuel.m@gmail.com>
2017-06-27 14:40:22 -0600

    added packages to house database and local key management functionality

---
+++ src/bluepea/db/__init__.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+"""
+DB package
+
+"""
+from __future__ import generator_stop
+
+
+
---
+++ src/bluepea/keep/keeping.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+"""
+Keeping Module
+
+"""
+from __future__ import generator_stop
+
+from collections import OrderedDict as ODict, deque
+import enum
+
+from ioflo.aid.sixing import *
+from ioflo.aid import getConsole
+
+console = getConsole()
+
