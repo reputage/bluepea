ab65200dc4320da5fe0391fc5453a318914660aa
Samuel M Smith <smith.samuel.m@gmail.com>
2017-06-28 13:12:19 -0600

    Added test code for key creation and storage signing

--- src/bluepea/help/helping.py
+++ src/bluepea/help/helping.py
@@ -5,11 +5,81 @@ Helping Module
 """
 from __future__ import generator_stop
 
+import os
+import stat
 from collections import OrderedDict as ODict, deque
 import enum
 
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+try:
+    import msgpack
+except ImportError:
+    pass
+
 from ioflo.aid.sixing import *
 from ioflo.aid import getConsole
+from ioflo.aid import filing
 
 console = getConsole()
 
+
+def dumpKeys(data, filepath):
+    '''
+    Write data as as type self.ext to filepath. json or msgpack
+    '''
+    if ' ' in filepath:
+        raise IOError("Invalid filepath '{0}' "
+                                "contains space".format(filepath))
+
+    perm_other = stat.S_IROTH | stat.S_IWOTH | stat.S_IXOTH
+    perm_group = stat.S_IRGRP | stat.S_IWGRP | stat.S_IXGRP
+    cumask = os.umask(perm_other | perm_group)  # save old into cumask and set new
+
+    root, ext = os.path.splitext(filepath)
+    if ext == '.json':
+        with filing.ocfn(filepath, "w+") as f:
+            json.dump(data, f, indent=2)
+            f.flush()
+            os.fsync(f.fileno())
+    elif ext == '.msgpack':
+        if not msgpack:
+            raise IOError("Invalid filepath ext '{0}' "
+                        "needs msgpack installed".format(filepath))
+        with filing.ocfn(filepath, "w+b", binary=True) as f:
+            msgpack.dump(data, f)
+            f.flush()
+            os.fsync(f.fileno())
+    else:
+        raise IOError("Invalid filepath ext '{0}' "
+                    "not '.json' or '.msgpack'".format(filepath))
+
+    os.umask(cumask)  # restore old
+
+
+def loadKeys(filepath):
+    '''
+    Return data read from filepath as converted json
+    Otherwise return None
+    '''
+    try:
+        root, ext = os.path.splitext(filepath)
+        if ext == '.json':
+            with filing.ocfn(filepath, "r") as f:
+                it = json.load(f, object_pairs_hook=ODict)
+        elif ext == '.msgpack':
+            if not msgpack:
+                raise IOError("Invalid filepath ext '{0}' "
+                            "needs msgpack installed".format(filepath))
+            with filing.ocfn(filepath, "rb", binary=True) as f:
+                it = msgpack.load(f, object_pairs_hook=ODict)
+        else:
+            it = None
+    except EOFError:
+        return None
+    except ValueError:
+        return None
+    return it
--- test/end/test_ending.py
+++ test/end/test_ending.py
@@ -1,5 +1,11 @@
 from __future__ import generator_stop
 
+import os
+import tempfile
+import shutil
+import binascii
+import base64
+
 from collections import OrderedDict as ODict
 try:
     import simplejson as json
@@ -12,6 +18,7 @@ import falcon
 import pytest
 from pytest import approx
 import pytest_falcon # declares client fixture
+
 """
     PyTest fixtures are registered globally in the pytest package
     So any test function can accept a fixture as a parameter supplied by
@@ -21,7 +28,7 @@ import pytest_falcon # declares client fixture
 """
 
 import bluepea.end.ending as ending
-
+from bluepea.help.helping import dumpKeys, loadKeys
 
 store = storing.Store(stamp=0.0)
 
@@ -66,3 +73,86 @@ def test_post_AgentRegister(client):  # client is a fixture in pytest_falcon
                         'sig': 'ABCDEFGHIJKLMNOPQRSTUVWZYZ0123456789'}
     print("Done Test")
 
+
+def setupBaseDir(baseDirPath=""):
+    """
+    Create temporary directory
+    """
+    # create temp directory at /tmp/bluepea...test
+    if not baseDirPath:
+        baseDirPath = tempfile.mkdtemp(prefix="bluepea",  suffix="test", dir="/tmp")
+    baseDirPath = os.path.abspath(os.path.expanduser(baseDirPath))
+    return baseDirPath
+
+
+def cleanupBaseDir(baseDirPath):
+    """
+    Remove temporary baseDirPath
+    """
+    if os.path.exists(baseDirPath):
+        shutil.rmtree(baseDirPath)
+
+
+
+def test_post_AgentRegisterSigned(client):  # client is a fixture in pytest_falcon
+    """
+    Use libnacl and Base64 to generate compliant signed Agent Registration
+    """
+    print("Testing POST /agent/register with signature")
+
+    import libnacl
+    import libnacl.sign
+
+    signer = signer = libnacl.sign.Signer()  # creates signing/verification key pair
+    sigkey = signer.sk  # 64 byte private signing key
+    verkey = signer.vk  # 32 byte public verification key
+    seed = signer.seed  # random seed used to generate private signing key
+    assert seed == signer.sk[:32]
+
+    vk, sk = libnacl.crypto_sign_seed_keypair(seed)  # regenerate keypair from seed
+    assert sk == sigkey
+    assert vk == verkey
+
+    baseDirPath = setupBaseDir()
+    assert baseDirPath.startswith("/tmp/bluepea")
+    assert baseDirPath.endswith("test")
+    keyDirPath = os.path.join(baseDirPath, "keys")
+    os.makedirs(keyDirPath)
+    assert os.path.exists(keyDirPath)
+    keyFilePath = os.path.join(keyDirPath, "signer.json")
+    assert keyFilePath.endswith("keys/signer.json")
+
+    keyData = ODict(seed=binascii.hexlify(seed).decode('utf-8'),
+                    sigkey=binascii.hexlify(sigkey).decode('utf-8'),
+                    verkey=binascii.hexlify(verkey).decode('utf-8'))
+
+    dumpKeys(keyData, keyFilePath)
+    assert os.path.exists(keyFilePath)
+
+    keyDataFiled = loadKeys(keyFilePath)
+    assert keyData == keyDataFiled
+
+    sd = binascii.unhexlify(keyDataFiled['seed'].encode('utf-8'))
+    assert sd == seed
+    sk = binascii.unhexlify(keyDataFiled['sigkey'].encode('utf-8'))
+    assert sk == sigkey
+    vk = binascii.unhexlify(keyDataFiled['verkey'].encode('utf-8'))
+    assert vk == verkey
+
+
+    headers = {"Content-Type": "text/html; charset=utf-8",
+               "Signature": "ABCDEFGHIJKLMNOPQRSTUVWZYZ0123456789", }
+    body = json.dumps(dict(did="did:igo:abcdefghijklmnopqrstuvwxyz",
+                           signer="did:igo:abcdefghijklmnopqurstuvwxyz#0"))
+    rep = client.post('/agent/register', body=body, headers=headers)
+    assert rep.status == falcon.HTTP_OK
+    assert rep.headers == {'content-length': '153', 'content-type': 'application/json; charset=UTF-8'}
+    assert rep.body == ('{"data": {"did": "did:igo:abcdefghijklmnopqrstuvwxyz", "signer": '
+                        '"did:igo:abcdefghijklmnopqurstuvwxyz#0"}, "sig": '
+                        '"ABCDEFGHIJKLMNOPQRSTUVWZYZ0123456789"}')
+    assert rep.json == {'data': {'did': 'did:igo:abcdefghijklmnopqrstuvwxyz',
+                        'signer': 'did:igo:abcdefghijklmnopqurstuvwxyz#0'},
+                        'sig': 'ABCDEFGHIJKLMNOPQRSTUVWZYZ0123456789'}
+
+    cleanupBaseDir(baseDirPath)
+    print("Done Test")
