54c32967d16428f12f073b598573ff4e7fe9182a
Samuel M Smith <smith.samuel.m@gmail.com>
2017-06-30 15:07:49 -0600

    added db support functions

--- src/bluepea/db/dbing.py
+++ src/bluepea/db/dbing.py
@@ -5,11 +5,67 @@ DBing Module
 """
 from __future__ import generator_stop
 
+import os
 from collections import OrderedDict as ODict, deque
 import enum
+
+import lmdb
 
 from ioflo.aid.sixing import *
 from ioflo.aid import getConsole
 
 console = getConsole()
 
+MAX_DB_COUNT = 8
+
+BASE_DIR_PATH = "/var/db/bluepea"  # default
+ALT_BASE_DIR_PATH = os.path.join('~', '.bluepea')
+
+dbBaseDirPath = None  # database directory location has not been set up yet
+dbEnv = None  # database environment has not been set up yet
+
+def setupDbEnv(baseDirPath=None):
+    """
+    Setup  the module globals dbBaseDirPath, dbBaseFilePath, dbEnv using baseDirPath
+    if provided otherwise use BASE_DIR_PATH
+
+    """
+    global dbBaseDirPath, dbBaseFilePath, dbEnv
+
+    if not baseDirPath:
+        baseDirPath = BASE_DIR_PATH
+
+    baseDirPath = os.path.abspath(os.path.expanduser(baseDirPath))
+    if not os.path.exists(baseDirPath):
+        try:
+            os.makedirs(baseDirPath)
+        except OSError as ex:
+            baseDirPath = ALT_BASE_DIR_PATH
+            baseDirPath = os.path.abspath(os.path.expanduser(baseDirPath))
+            if not os.path.exists(baseDirPath):
+                os.makedirs(baseDirPath)
+    else:
+        if not os.access(baseDirPath, os.R_OK | os.W_OK):
+            baseDirPath = ALT_BASE_DIR_PATH
+            baseDirPath = os.path.abspath(os.path.expanduser(baseDirPath))
+            if not os.path.exists(baseDirPath):
+                os.makedirs(baseDirPath)
+
+    dbBaseDirPath = baseDirPath  # set global
+
+    #dbBaseFilePath = os.path.join(dbBaseDirPath, "bluepea.lmdb")  # set global
+
+    dbEnv = lmdb.open(dbBaseDirPath, max_dbs=MAX_DB_COUNT)
+    # creates files data.mdb and lock.mdb in dbBaseDirPath
+
+    # create named dbs  (core and tables)
+    dbEnv.open_db(b'core')
+
+    return dbEnv
+
+#with env.begin(write=True) as txn:
+   #txn.put('somename', 'somedata')
+
+if __name__ == '__main__':
+    env = setupDbEnv()
+    print("Setup dbEnv")
---
+++ test/db/test_dbing.py
@@ -0,0 +1,76 @@
+from __future__ import generator_stop
+
+import os
+import stat
+import tempfile
+import shutil
+import binascii
+import base64
+
+from collections import OrderedDict as ODict
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+import libnacl
+import libnacl.sign
+
+import pytest
+from pytest import approx
+
+from bluepea.help.helping import setupTmpBaseDir, cleanupBaseDir
+from bluepea.db import dbing
+
+
+def test_setupDbEnv():
+    """
+
+    """
+    print("Testing Setup DB Env")
+
+    baseDirPath = setupTmpBaseDir()
+    assert baseDirPath.startswith("/tmp/bluepea")
+    assert baseDirPath.endswith("test")
+    dbDirPath = os.path.join(baseDirPath, "db/bluepea")
+    os.makedirs(dbDirPath)
+    assert os.path.exists(dbDirPath)
+
+    env = dbing.setupDbEnv(baseDirPath=dbDirPath)
+    assert env.path() == dbDirPath
+
+    assert dbing.dbBaseDirPath == dbDirPath
+    assert dbing.dbEnv is env
+
+
+
+    data = ODict()
+
+    dbCore = dbing.dbEnv.open_db(b'core')  # open named sub db named 'core' within env
+
+    with dbing.dbEnv.begin(db=dbCore, write=True) as txn:  # txn is a Transaction object
+        data["name"] = "John Smith"
+        data["city"] = "Alta"
+        datab = json.dumps(data, indent=2).encode("utf-8")
+        txn.put(b'person0', datab)  # keys and values are bytes
+        d0b = txn.get(b'person0')
+        assert d0b == datab
+
+        data["name"] = "Betty Smith"
+        data["city"] = "Snowbird"
+        datab = json.dumps(data, indent=2).encode("utf-8")
+        txn.put(b'person1', datab)  # keys and values are bytes
+        d1b = txn.get(b'person1')
+        assert d1b == datab
+
+        d0b = txn.get(b'person0')  # re-fetch person0
+        assert d0b != datab
+        data = json.loads(d0b.decode('utf-8'), object_pairs_hook=ODict)
+        assert data['name'] == "John Smith"
+        assert data['city'] == "Alta"
+
+
+    cleanupBaseDir(baseDirPath)
+    assert not os.path.exists(dbDirPath)
+    print("Done Test")
+
